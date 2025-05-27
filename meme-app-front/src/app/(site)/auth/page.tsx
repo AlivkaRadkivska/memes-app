984ec58821e97efe9320da9401d9b9dce641980b
AlivkaRadkivska <aradkivska@gmail.com>
2025-05-28 00:20:00 +0300

    chore:  configure docker files for deploy

--- meme-app-front/src/app/(site)/auth/page.tsx
+++ meme-app-front/src/app/(site)/auth/page.tsx
@@ -1,11 +1,14 @@
 import AuthForm from '@/components/auth/auth-form';
 import { UserSidebar } from '@/components/site-layout/user-sidebar';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
   return (
     <div className="min-h-[calc(100vh-60px)] flex px-4 py-0 sm:px-0">
       <div className="w-full flex items-start justify-center px-4 pt-6 pb-2 sm:px-6 lg:px-8">
-        <AuthForm />
+        <Suspense>
+          <AuthForm />
+        </Suspense>
       </div>
 
       <UserSidebar />
